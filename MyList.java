a612a60a35ef89ff1d96797c5d3b4eaff11a2949
Student <student@lab-311-020.cs.csubak.edu>
2019-02-18 17:18:48 -0800

    added more to MyList.java

--- MyList.java
+++ MyList.java
@@ -1,30 +1,97 @@
-public class MyList<T> ()
+public class MyList<T> implements Iterable<T>
 {
-	public class Node<T> ()
+	protected Node head, tail;
+	protected size;
+
+	protected class Node
 	{
-		public:
-			T data;
-			Node next, previous;
-		Node<T>() {
-			data = 0;
-			next = previous = null;
+		T data;
+		Node next, previous;
+
+		public Node() {
+			Node(0, null, null);
 		}
-		Node<T>(T d, Node p, Node n) {
-			data = 0;
-			next = n;
-			previous = p;
+		public Node(T d, Node n, Node p) {
+			this.data = d;
+			this.next = n;
+			this.previous = p;
 		}
 	}
 
-	public class ListIterator implements Iterator<T> 
-	{
-		public ListIterator( boolean dir );
-		protected Node currentNode;
-		protected bool forward;
-		public boolean hasNext();
-		public T next();
-		public void remove();	
+	protected class ListIterator implements Iterator<T>
+   	{
+		Node current;
+		boolean forward;
+
+		public ListIterator(boolean forward) {
+			this.forward = forward;
+			current = forward ? head : tail;
+		}
+
+		protected Node currentNode() {
+			return this.current;
+		}
+
+		protected boolean forward() {
+			return this.forward;
+		}
+
+		public boolean hasNext() {
+			return current.next != null;
+		}
+
+		public T next() {
+			return current.next;
+		}
+
+		public void remove() {
+			this.previous.next = this.next;
+			this.next.previous = this.previous;
+			this.next = this.previous = null;
+		}
+
 	}
 
-	public class 
+	public MyList() {
+		MyList(0, null, null);
+	}
+
+	public MyList(int s, Node h, Node t) {
+		this.size = s;
+		this.head = h;
+		this.tail = t;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public int getSize() {
+		return this.size;
+	}
+
+	public void insert(T obj, int pos) {
+		if(isEmpty()){
+			head = tail = new Node(size, null, null);
+		} else {
+			for(int i = 0; i < size; ++i) {
+			/*
+			 *TODO: finish for loop
+			 * */	
+			}
+		}
+	}
+
+	/*
+	 *TODO: Add insert and remove
+	 */
+
+	public Iterator<t> iterator() {
+		return iterator(true);
+	}
+
+	public Iterator<T> iterator(boolean forward) {
+		return new ListIterator(forward);
+	}
+
 }
---
+++ MyStack.java
@@ -0,0 +1,10 @@
+public class MyStack<T> extends MyList<T>
+{
+	public MyStack() {
+		super();
+	}
+
+	public void push(T obj) {
+		
+	}
+}
